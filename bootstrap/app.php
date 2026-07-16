a603a8b2422499f60a0d09186a69467b4ad03077
Nama Anda <faizalba74@gmail.com>
2026-07-16 17:01:46 +0700

    Fix: trust all proxies in middleware to enable HTTPS assets behind Cloudflare Tunnel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         // Tambahkan sanctum untuk API
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
